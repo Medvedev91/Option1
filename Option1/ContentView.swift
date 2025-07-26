ea64316c32401845dcf4849cd84ad9f748c12b48
Ivan Medvedev <hi@timeto.me>
2025-07-26 09:05:30 +0300

    ContentView.swift:

--- Option1/ContentView.swift
+++ Option1/ContentView.swift
@@ -1,17 +1,35 @@
 import SwiftUI
+import Cocoa
 
 struct ContentView: View {
+    
+    @State private var axuiElements: [AXUIElement] = []
+    @State private var focusedWindow: AXUIElement? = nil
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Button("Get") {
+                Task {
+                    try await Task.sleep(nanoseconds: 3_000_000_000)
+                    let axuiElements = try! WindowsManager.getWindowsForActiveApplicationOrNil() ?? []
+                    self.axuiElements = axuiElements
+                    self.focusedWindow = try! WindowsManager.getFocusedWindowOrNil()
+                }
+            }
+            
+            if let focusedWindow = focusedWindow {
+                Button("Focused \(try! focusedWindow.title()) \(focusedWindow.id())") {
+                    try! WindowsManager.focusWindow(axuiElement: focusedWindow)
+                }
+            }
+            
+            ForEach(axuiElements, id: \.self) { axuiElement in
+                Button("Run \(try! axuiElement.title()) \(axuiElement.id())") {
+                    try! WindowsManager.focusWindow(axuiElement: axuiElement)
+                }
+            }
         }
         .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Thank you https://github.com/lwouis/alt-tab-macos! Without it, I couldn't do anything.
+Thanks https://github.com/lwouis/alt-tab-macos!
