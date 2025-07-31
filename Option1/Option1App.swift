b0d9634e5038711877f685e5ae4d48e53f3eb1e5
Ivan Medvedev <hi@timeto.me>
2025-07-31 15:24:21 +0300

    create-dmg

--- Option1/Option1App.swift
+++ Option1/Option1App.swift
@@ -5,8 +5,8 @@
 // - Bump app version,
 // - Xcode: Product -> Archive -> Distribute App -> Direct Distribution;
 // - Wait for Ready to distribute -> Export App. Save to Download with New Folder -> "Option1";
-// - Open Disk Utility app, New Image -> Image from Folder -> create .dmg from Option1 folder;
-// - Move Option1.dmg to Option1 folder, remove Option1 application file from Option1 folder.
+// - Open Terminal app, cd ~/Downloads/Option1; run: create-dmg 'Option1.app', don't care about signed error;
+// - Rename new file to Option1.dmg, remove Option1.app file from Option1 folder.
 // - Right click on Sparkle in project navigator: Show in Finder;
 // - Go to parent folder: Go -> Enclosing Folder -> Artifacts -> sparkle -> Sparkle -> bin;
 // - Open Terminal app, move generate_appcast file to terminal, add ~/Downloads/Option1, run;
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ https://github.com/lwouis/alt-tab-macos
 https://github.com/soffes/HotKey
 
 https://github.com/sparkle-project/Sparkle
+
+https://github.com/sindresorhus/create-dmg
