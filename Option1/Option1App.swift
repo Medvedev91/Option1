be6cb3e36b56e8bc88cd352260664db8727f72ff
Ivan Medvedev <hi@timeto.me>
2025-08-01 13:14:26 +0300

    Sparkle.swift

--- Option1/Option1App.swift
+++ Option1/Option1App.swift
@@ -1,31 +1,9 @@
-//
-// Sparkle https://sparkle-project.org/documentation/
-//
-// Release Documentation
-// - Bump app version,
-// - Xcode: Product -> Archive -> Distribute App -> Direct Distribution;
-// - Wait for Ready to distribute -> Export App. Save to Download with New Folder -> "Option1";
-// - Open Terminal app, cd ~/Downloads/Option1; run: create-dmg 'Option1.app', don't care about signed error;
-// - Rename new file to Option1.dmg, remove Option1.app file from Option1 folder.
-// - Right click on Sparkle in project navigator: Show in Finder;
-// - Go to parent folder: Go -> Enclosing Folder -> Artifacts -> sparkle -> Sparkle -> bin;
-// - Open Terminal app, move generate_appcast file to terminal, add ~/Downloads/Option1, run;
-// - Upload appcast.xml and Option1.dmg to option1.io root.
-//
-
 import SwiftUI
-import Sparkle
-
-var sparkleController: SPUStandardUpdaterController!
 
 @main
 struct Option1App: App {
     
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    
-    init() {
-        sparkleController = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
-    }
     
     var body: some Scene {
         WindowGroup {
