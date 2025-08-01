9e90c9b1dd872aca50c0321125ba2590bccf6737
Ivan Medvedev <hi@timeto.me>
2025-08-01 13:10:37 +0300

    Use PermissionView()

--- Option1/UI/MainScreen.swift
+++ Option1/UI/MainScreen.swift
@@ -6,41 +6,43 @@ struct MainScreen: View {
     @State private var axuiElements: [AXUIElement] = []
     @State private var focusedWindow: AXUIElement? = nil
     
+    private let timer1s = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var isPermissionGranted = isAccessibilityGranted(showDialog: false)
+    
     var body: some View {
         VStack {
             
-            Button("Debug App in 3 Seconds") {
-                Task {
-                    try await Task.sleep(nanoseconds: 3_000_000_000)
-                    let axuiElements = try! WindowsManager.getWindowsForActiveApplicationOrNil() ?? []
-                    self.axuiElements = axuiElements
-                    self.focusedWindow = try! WindowsManager.getFocusedWindowOrNil()
+            if !isPermissionGranted {
+                PermissionView()
+            } else {
+                
+                Button("Debug App in 3 Seconds") {
+                    Task {
+                        try await Task.sleep(nanoseconds: 3_000_000_000)
+                        let axuiElements = try! WindowsManager.getWindowsForActiveApplicationOrNil() ?? []
+                        self.axuiElements = axuiElements
+                        self.focusedWindow = try! WindowsManager.getFocusedWindowOrNil()
+                    }
                 }
-            }
-            
-            if let focusedWindow = focusedWindow {
-                Button("Focused \(try! focusedWindow.title()) \(focusedWindow.id())") {
-                    try! WindowsManager.focusWindow(axuiElement: focusedWindow)
+                
+                if let focusedWindow = focusedWindow {
+                    Button("Focused \(try! focusedWindow.title()) \(focusedWindow.id())") {
+                        try! WindowsManager.focusWindow(axuiElement: focusedWindow)
+                    }
                 }
-            }
-            
-            ForEach(axuiElements, id: \.self) { axuiElement in
-                Button("Run \(try! axuiElement.title()) \(axuiElement.id())") {
-                    try! WindowsManager.focusWindow(axuiElement: axuiElement)
+                
+                ForEach(axuiElements, id: \.self) { axuiElement in
+                    Button("Run \(try! axuiElement.title()) \(axuiElement.id())") {
+                        try! WindowsManager.focusWindow(axuiElement: axuiElement)
+                    }
                 }
+                
+                SparkleButtonView()
             }
-            
-            SparkleButtonView()
         }
         .padding()
-        .onAppear {
-            print("\(promptForAccessibilityRequirements())")
+        .onReceive(timer1s) { _ in
+            isPermissionGranted = isAccessibilityGranted(showDialog: false)
         }
     }
-}
-
-private func promptForAccessibilityRequirements() -> Bool {
-    let checkOptPrompt = kAXTrustedCheckOptionPrompt.takeUnretainedValue() as NSString
-    let options = [checkOptPrompt: false]
-    return AXIsProcessTrustedWithOptions(options as CFDictionary?)
 }
