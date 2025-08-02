523e49ad872cac1fedfaa29cd830a8b80c4b5267
Ivan Medvedev <hi@timeto.me>
2025-08-02 20:47:11 +0300

    AppDelegate.swift: Use checkForUpdatesInBackground()

--- Option1/AppDelegate.swift
+++ Option1/AppDelegate.swift
@@ -2,6 +2,14 @@ import AppKit
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        // In logs we see "Warning: Background app automatically schedules for update checks but does
+        // not implement gentle reminders...https://sparkle-project.org/documentation/gentle-reminders/".
+        // That is because Option 1 is "dockless" app - app that does not appear in the Dock.
+        // Dockless app do not show alerts about updates automatically. Call it manually.
+        sparkleController.updater.checkForUpdatesInBackground()
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         setupHotKeys()
     }
