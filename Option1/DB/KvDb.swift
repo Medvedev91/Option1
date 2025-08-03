c092ce4b0d43ff54c46130cbb44549f1973bbfc8
Ivan Medvedev <hi@timeto.me>
2025-08-03 20:47:14 +0300

    KvDb.swift

---
+++ Option1/DB/KvDb.swift
@@ -0,0 +1,13 @@
+import SwiftData
+
+@Model
+class KvDb {
+    
+    @Attribute(.unique) var key: String
+    var value: String
+    
+    init (key: String, value: String) {
+        self.key = key
+        self.value = value
+    }
+}
