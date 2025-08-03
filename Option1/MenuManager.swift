86e179e514f43c85ef246447e96a5e31f57474a5
Ivan Medvedev <hi@timeto.me>
2025-08-03 12:18:11 +0300

    Init Workspaces

---
+++ Option1/MenuManager.swift
@@ -0,0 +1,45 @@
+//
+// Create Image Example
+//
+// var checkMark = NSImage(systemSymbolName: "gearshape", accessibilityDescription: "Checked")!
+// let config = NSImage.SymbolConfiguration(pointSize: 18, weight: .semibold, scale: .small)
+// checkMark = checkMark.withSymbolConfiguration(config)!
+//
+
+import AppKit
+
+private let statusBar: NSStatusBar = NSStatusBar.system
+private let statusItem: NSStatusItem = statusBar.statusItem(
+    withLength: NSStatusItem.variableLength
+)
+private let statusMenu = NSMenu(title: "Option 1")
+
+class MenuManager {
+    
+    static func setup() {
+        statusItem.button?.title = "Shared"
+        statusItem.menu = statusMenu
+        
+        let i1 = statusMenu.addItem(
+            withTitle: "Shared",
+            action: #selector(AppDelegate.openSettings),
+            keyEquivalent: ""
+        )
+        i1.state = .on
+        
+        statusMenu.addItem(NSMenuItem.separator())
+        
+        statusMenu.addItem(
+            withTitle: "Settings             ", // Spaces to extra menu width
+            action: #selector(AppDelegate.openSettings),
+            keyEquivalent: ""
+        )
+    }
+}
+
+private extension AppDelegate {
+    
+    @objc func openSettings() {
+        print("openSettings")
+    }
+}
