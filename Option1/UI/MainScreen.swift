1aa52463397748d8a808a260a35d8f8ba417e8eb
Ivan Medvedev <hi@timeto.me>
2025-08-03 20:50:50 +0300

    MainScreen.swift: Workspaces UI

--- Option1/UI/MainScreen.swift
+++ Option1/UI/MainScreen.swift
@@ -1,7 +1,12 @@
 import SwiftUI
+import SwiftData
 import Cocoa
 
 struct MainScreen: View {
+    
+    @Environment(\.modelContext) var modelContext
+    
+    @Query private var workspacesDb: [WorkspaceDb] = []
     
     @State private var axuiElements: [AXUIElement] = []
     @State private var focusedWindow: AXUIElement? = nil
@@ -38,11 +43,27 @@ struct MainScreen: View {
                 }
                 
                 SparkleButtonView()
+                
+                Section("Workspaces") {
+                    ForEach(workspacesDb) { workspaceDb in
+                        Text(workspaceDb.name)
+                            .onTapGesture {
+                                modelContext.delete(workspaceDb)
+                            }
+                    }
+                }
+                
+                Button("New Workspace") {
+                    modelContext.insert(WorkspaceDb(id: UUID(), name: "New Workspace", date: Date.now, sort: 1))
+                }
             }
         }
         .padding()
         .onReceive(timer1s) { _ in
             isPermissionGranted = isAccessibilityGranted(showDialog: false)
+        }
+        .onChange(of: workspacesDb, initial: true) { _, workspacesDb in
+            MenuManager.setWorkspacesDb(workspacesDb)
         }
     }
 }
