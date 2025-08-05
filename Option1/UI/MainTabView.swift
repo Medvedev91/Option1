a201f81df21964d80789a965451e99e5fce8f940
Ivan Medvedev <hi@timeto.me>
2025-08-05 13:46:02 +0300

    MainTabView.swift:

--- Option1/UI/MainTabView.swift
+++ Option1/UI/MainTabView.swift
@@ -3,19 +3,21 @@ import SwiftUI
 struct MainTabView: View {
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            
-            Text("Bind Window - Option + Shift + 1 .. 9")
-                .textAlign(.leading)
-                .padding(.top, 4)
-
-            Text("Focus Window - Option + 1 .. 9")
-                .textAlign(.leading)
-                .padding(.top, 4)
-            
-            Spacer()
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                
+                Text("Bind Window - Option + Shift + 1 .. 9")
+                    .textAlign(.leading)
+                    .padding(.top, 4)
+                
+                Text("Focus Window - Option + 1 .. 9")
+                    .textAlign(.leading)
+                    .padding(.top, 4)
+                
+                Spacer()
+            }
+            .padding()
         }
         .navigationTitle("Option 1")
-        .padding()
     }
 }
