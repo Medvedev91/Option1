c44a0e65c0c9bb8eb6f335ca36f19cd638e889ff
Ivan Medvedev <hi@timeto.me>
2025-08-05 20:14:41 +0300

    AppScreen.swift: Ordering

--- Option1/UI/AppScreen.swift
+++ Option1/UI/AppScreen.swift
@@ -6,7 +6,7 @@ struct AppScreen: View {
     
     @Environment(\.modelContext) private var modelContext
     
-    @Query private var workspacesDb: [WorkspaceDb] = []
+    @Query(sort: \WorkspaceDb.sort) private var workspacesDb: [WorkspaceDb] = []
     
     private let timer1s = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     @State private var isPermissionGranted = isAccessibilityGranted(showDialog: false)
