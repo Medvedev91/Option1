bf41bae1eb683f982c5f49ab9e0b487caa079985
Ivan Medvedev <hi@timeto.me>
2025-08-05 19:48:39 +0300

    WorkspacesTabView.swift:

--- Option1/UI/WorkspacesTabView.swift
+++ Option1/UI/WorkspacesTabView.swift
@@ -1,10 +1,32 @@
 import SwiftUI
+import SwiftData
 
 struct WorkspacesTabView: View {
     
+    @Environment(\.modelContext) private var modelContext
+    
+    @Query private var workspacesDb: [WorkspaceDb] = []
+    
     var body: some View {
-        ScrollView {
-            VStack {
+        List {
+            Section {
+                ForEach(workspacesDb) { workspaceDb in
+                    Text(workspaceDb.name)
+                        .contextMenu {
+                            Button(
+                                action: {
+                                    modelContext.delete(workspaceDb)
+                                },
+                                label: {
+                                    Text("Delete")
+                                        .foregroundColor(.red)
+                                }
+                            )
+                        }
+                        .padding(.vertical, 4)
+                }
+                .onMove { from, to in
+                }
             }
         }
         .navigationTitle("Workspaces")
@@ -12,7 +34,7 @@ struct WorkspacesTabView: View {
             ToolbarItem(placement: .primaryAction) {
                 Button("New Workspace") {
                 }
-                .buttonStyle(.bordered)
+                .buttonStyle(.link)
             }
         }
     }
