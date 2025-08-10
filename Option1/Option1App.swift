e36f1c3efc8222b538b6752d255b7a4e8cb4d064
Ivan Medvedev <hi@timeto.me>
2025-08-10 16:11:56 +0300

    modelContainer builder

--- Option1/Option1App.swift
+++ Option1/Option1App.swift
@@ -1,16 +1,24 @@
 import SwiftUI
 import SwiftData
 
+// https://fatbobman.com/en/snippet/fix-synchronization-issues-for-macos-apps-using-core-dataswiftdata/
+
 @main
 struct Option1App: App {
     
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
+    private let modelContainer: ModelContainer = {
+        let schema = Schema([KvDb.self, WorkspaceDb.self])
+        let configuration = ModelConfiguration(cloudKitDatabase: .none)
+        return try! ModelContainer(for: schema, configurations: configuration)
+    }()
+    
     var body: some Scene {
         WindowGroup {
             AppScreen()
         }
-        .modelContainer(for: [KvDb.self, WorkspaceDb.self])
+        .modelContainer(modelContainer)
         .commands {
             // Disable creating new window by Command + N
             CommandGroup(replacing: .newItem) {}
