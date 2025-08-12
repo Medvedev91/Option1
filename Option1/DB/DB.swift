24e8c0d922af007c7e735e76e1c74bc6d10530f8
Ivan Medvedev <hi@timeto.me>
2025-08-12 22:18:47 +0300

    DB storage url

--- Option1/DB/DB.swift
+++ Option1/DB/DB.swift
@@ -1,3 +1,4 @@
+import Foundation
 import SwiftData
 
 // https://fatbobman.com/en/snippet/fix-synchronization-issues-for-macos-apps-using-core-dataswiftdata/
@@ -5,8 +6,16 @@ import SwiftData
 struct DB {
     
     static let modelContainer: ModelContainer = {
+        // https://gist.github.com/pdarcey/981b99bcc436a64df222cd8e3dd92871
+        let bundle: String = Bundle.main.bundleIdentifier!
+        let fileURL = URL.applicationSupportDirectory.appending(path: "\(bundle)/SwiftData.store")
         let schema = Schema([KvDb.self, WorkspaceDb.self, BindDb.self])
-        let configuration = ModelConfiguration(cloudKitDatabase: .none)
+        let configuration = ModelConfiguration(
+            "SwiftData",
+            schema: schema,
+            url: fileURL,
+            cloudKitDatabase: .none,
+        )
         return try! ModelContainer(for: schema, configurations: configuration)
     }()
     
