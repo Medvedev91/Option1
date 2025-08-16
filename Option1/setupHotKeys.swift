a6dc21f31c015b6f688d459fc094b8c79f3d73ca
Ivan Medvedev <hi@timeto.me>
2025-08-16 22:32:02 +0300

    setupHotKeys.swift:

--- Option1/setupHotKeys.swift
+++ Option1/setupHotKeys.swift
@@ -3,33 +3,12 @@ import HotKey
 
 // todo rename to HotKeysUtils
 
-private var numberKeys: [Key: [UUID? /* Workspace id or shared */: AXUIElement?]] = [
-    .one: [:], .two: [:], .three: [:], .four: [:], .five: [:],
-    .six: [:], .seven: [:], .eight: [:], .nine: [:], .zero: [:],
-]
+private let keys: [Key] = [.one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .zero]
 
 private var keepHotKeyHandlers: [Any] = []
 
 func setupHotKeys() {
-    numberKeys.forEach { key, axui in
-        
-        keepHotKeyHandlers.append(
-            HotKey(
-                key: key,
-                modifiers: [.option, .shift],
-                keyDownHandler: {
-                    _ = isAccessibilityGranted(showDialog: true)
-                    do {
-                        let workspaceDb = MenuManager.workspaceDb
-                        numberKeys[key]![workspaceDb?.id] = try WindowsManager.getFocusedWindowOrNil()
-                        print("pin \(try WindowsManager.getFocusedWindowOrNil()?.id())")
-                    } catch {
-                        reportApi("getFocusedWindowOrNil() error:\(error)")
-                    }
-                }
-            )
-        )
-        
+    keys.forEach { key in
         keepHotKeyHandlers.append(
             HotKey(
                 key: key,
@@ -38,7 +17,7 @@ func setupHotKeys() {
                     Task { @MainActor in
                         handleRun(key: key)
                     }
-                }
+                },
             )
         )
     }
@@ -46,7 +25,6 @@ func setupHotKeys() {
 
 @MainActor
 private func handleRun(key: Key) {
-    print("handleRun() \(key)")
     _ = isAccessibilityGranted(showDialog: true)
     
     let bindsDb = BindDb.selectAll()
@@ -73,42 +51,26 @@ private func handleRun(key: Key) {
         return
     }
     
-    // todo do catch
     let pid = runningApplication.processIdentifier
     let axElement = AXUIElementCreateApplication(pid)
-    let windows = try! axElement.allWindows(pid)
     
-    guard let window = windows.first(where: {
-        try! $0.title()!.lowercased().contains(bindDb.substring.lowercased())
-    }) else { return }
-    
-    try! WindowsManager.focusWindow(axuiElement: window)
-    // Fix Corner Case:
-    // - bind Telegram app (option + shift + 1),
-    // - press cmd + w to close the window,
-    // - press option + 1 to open.
-    // Telegram's window won't open.
-    if try! WindowsManager.getFocusedWindowOrNil() == nil {
-        WindowsManager.focusActiveApplication()
-    }
-    
-    /*
-    let workspaceAxui: AXUIElement? = numberKeys[key]![MenuManager.workspaceDb?.id] ?? nil
-    let sharedAxui: AXUIElement? = numberKeys[key]![nil] ?? nil
-    if let axui = (workspaceAxui ?? sharedAxui) {
-        do {
-            try WindowsManager.focusWindow(axuiElement: axui)
-            // Fix Corner Case:
-            // - bind Telegram app (option + shift + 1),
-            // - press cmd + w to close the window,
-            // - press option + 1 to open.
-            // Telegram's window won't open.
-            if try WindowsManager.getFocusedWindowOrNil() == nil {
-                WindowsManager.focusActiveApplication()
-            }
-        } catch {
-            reportApi("focusWindow() error:\(error)")
+    do {
+        let windows = try axElement.allWindows(pid)
+        
+        guard let window = try windows.first(where: {
+            try $0.title()!.lowercased().contains(bindDb.substring.lowercased())
+        }) else { return }
+        
+        try WindowsManager.focusWindow(axuiElement: window)
+        // Fix Corner Case:
+        // - bind Telegram app (option + shift + 1),
+        // - press cmd + w to close the window,
+        // - press option + 1 to open.
+        // Telegram's window won't open.
+        if try WindowsManager.getFocusedWindowOrNil() == nil {
+            WindowsManager.focusActiveApplication()
         }
+    } catch {
+        reportApi("handleRun() error:\(error.localizedDescription)")
     }
-    */
 }
