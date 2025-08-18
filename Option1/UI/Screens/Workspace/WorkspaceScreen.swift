bfd6f4fd119abb7152c33ac6205be0ff2989830e
Ivan Medvedev <hi@timeto.me>
2025-08-19 02:16:00 +0300

    WorkspaceScreen.swift: Edit Workspace Name

--- Option1/UI/Screens/Workspace/WorkspaceScreen.swift
+++ Option1/UI/Screens/Workspace/WorkspaceScreen.swift
@@ -4,6 +4,13 @@ struct WorkspaceScreen: View {
     
     let workspaceDb: WorkspaceDb?
     
+    ///
+    
+    // Fix WTF bug - name is empty on second form open.
+    @State private var formId = UUID()
+    @State private var formPresented = false
+    @State private var formName = ""
+    
     var body: some View {
         List {
             ForEach(HotKeysUtils.keys, id: \.self) { key in
@@ -16,6 +23,34 @@ struct WorkspaceScreen: View {
             }
         }
         .listStyle(.plain)
+        .alert("", isPresented: $formPresented) {
+            TextField("Workspace", text: $formName)
+            Button("Cancel") {
+            }
+            .keyboardShortcut(.cancelAction)
+            Button("Save") {
+                workspaceDb?.updateName(formName)
+            }
+            .disabled(formName.isEmpty)
+            .keyboardShortcut(.defaultAction)
+        }
+        .id(formId)
         .navigationTitle(workspaceDb?.name ?? "Shared")
+        .toolbar {
+            if let workspaceDb = workspaceDb {
+                ToolbarItem(placement: .primaryAction) {
+                    RenameButton()
+                        .renameAction {
+                            showForm(workspaceDb: workspaceDb)
+                        }
+                }
+            }
+        }
+    }
+    
+    private func showForm(workspaceDb: WorkspaceDb) {
+        formId = UUID()
+        formName = workspaceDb.name
+        formPresented = true
     }
 }
