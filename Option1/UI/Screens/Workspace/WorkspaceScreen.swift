6aa1543c24eaf87cca4d786d378db8713600267f
Ivan Medvedev <hi@timeto.me>
2025-09-01 23:43:35 +0300

    WorkspaceScreen.swift: ActiveAppView()

--- Option1/UI/Screens/Workspace/WorkspaceScreen.swift
+++ Option1/UI/Screens/Workspace/WorkspaceScreen.swift
@@ -1,10 +1,13 @@
 import SwiftUI
+import Cocoa
 
 struct WorkspaceScreen: View {
     
     let workspaceDb: WorkspaceDb?
     
     ///
+    
+    @State private var activeAxuiElements: [AXUIElement] = []
     
     // Fix WTF bug - name is empty on second form open.
     @State private var formId = UUID()
@@ -13,12 +16,36 @@ struct WorkspaceScreen: View {
     
     var body: some View {
         ScrollView {
+            
             ForEach(HotKeysUtils.keys, id: \.self) { key in
                 WorkspaceBindView(
                     key: key,
                     workspaceDb: workspaceDb,
+                    onSelected: { bundle in
+                        guard let runningApplication = NSWorkspace.shared.runningApplications.first(where: {
+                            $0.bundleIdentifier?.lowercased() == bundle.lowercased()
+                        }) else { return }
+                        let pid = runningApplication.processIdentifier
+                        let axuiElement = AXUIElementCreateApplication(pid)
+                        do {
+                            let windows = try axuiElement.allWindows(pid)
+                            self.activeAxuiElements = windows
+                        } catch {
+                            reportApi("WorkspaceScreen onSelected() error: \(error)")
+                            return
+                        }
+                    },
                 )
                 .padding(.top, key == .one ? 8 : 0)
+            }
+            
+            Divider()
+                .padding()
+            
+            VStack(spacing: 8) {
+                ForEach(activeAxuiElements, id: \.self) { axuiElement in
+                    ActiveAppView(axuiElement: axuiElement)
+                }
             }
         }
         .alert("", isPresented: $formPresented) {
@@ -50,5 +77,27 @@ struct WorkspaceScreen: View {
         formId = UUID()
         formName = workspaceDb.name
         formPresented = true
+    }
+}
+
+private struct ActiveAppView: View {
+    
+    let axuiElement: AXUIElement
+    
+    ///
+    
+    private var title: String {
+        do {
+            return try axuiElement.title() ?? "--"
+        } catch {
+            reportApi("WorkspaceScreen ActiveAppView title error: \(error)")
+            return "--"
+        }
+    }
+    
+    var body: some View {
+        Text("\(title) #\(axuiElement.id() ?? 0)")
+            .textAlign(.leading)
+            .padding(.horizontal)
     }
 }
