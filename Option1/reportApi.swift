03df9108d8c9864f82832e8503328501b2197847
sergey@kurdy.uk <hi@timeto.me>
2025-11-13 15:23:54 +0300

    reportApi.swift: Http request

--- Option1/reportApi.swift
+++ Option1/reportApi.swift
@@ -1,4 +1,13 @@
+import Alamofire
+
 func reportApi(_ message: String) {
-    // todo
     print(message)
+    let parameters: [String: String] = [
+        "message": message,
+    ]
+    _ = AF.request(
+        "https://api.timeto.me/option1/report",
+        method: .post,
+        parameters: parameters
+    ).responseString { _ in }
 }
