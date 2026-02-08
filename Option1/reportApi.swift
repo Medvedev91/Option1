14fe8a8a3e1e02ad959e303f73143049a51c2593
Ivan <hi@timeto.me>
2026-02-08 12:28:32 +0300

    reportApi.swift:

--- Option1/reportApi.swift
+++ Option1/reportApi.swift
@@ -4,8 +4,9 @@ func reportApi(_ message: String) {
     reportLog("reportApi() \(message)")
     let parameters: [String: String] = [
         "message": message,
-        "model": getModelIdentifier() ?? "",
-        "os": getOsVersion(),
+        "build": SystemInfo.getBuildOrNil().map { "\($0)" } ?? "",
+        "model": SystemInfo.getModelIdentifierOrNil() ?? "",
+        "os": SystemInfo.getOsVersion(),
     ]
     _ = AF.request(
         "https://api.option1.io/report",
