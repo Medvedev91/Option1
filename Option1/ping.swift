d0dbe89c441b8bf31c0baa6363b1fe6fa047f828
Ivan <hi@timeto.me>
2026-02-08 08:43:02 +0300

    ping.swift

---
+++ Option1/ping.swift
@@ -0,0 +1,57 @@
+import Alamofire
+import SwiftyJSON
+
+private var lastPingTime: Int32 = 0
+private var isPingInProgress = false
+
+func ping() {
+    Task { @MainActor in
+        if isPingInProgress {
+            reportLog("ping() isPingInProgress")
+            return
+        }
+        if (lastPingTime + 4 * 3_600) > time() {
+            reportLog("ping() lastPingTime \(lastPingTime)")
+            return
+        }
+        reportLog("ping()")
+        let parameters: [String: String] = [
+            "token": KvDb.getTokenOrNil() ?? "",
+            "model": getModelIdentifier() ?? "",
+            "os": getOsVersion(),
+        ]
+        isPingInProgress = true
+        _ = AF.request(
+            "https://api.option1.io/ping",
+            method: .get,
+            parameters: parameters,
+        ).responseString { response in
+            switch response.result {
+            case .success(let jString):
+                guard let jData = jString.data(using: .utf8, allowLossyConversion: false) else {
+                    reportApi("ping() .success invalid data:\(jString)")
+                    break
+                }
+                guard let j = try? JSON(data: jData) else {
+                    reportApi("ping() .success invalid json:\(jString)")
+                    break
+                }
+                if j["status"] != "success" {
+                    reportApi("ping() .success not success:\(jString)")
+                    break
+                }
+                guard let token: String = j["data"]["token"].string else {
+                    reportApi("ping() .success no token:\(jString)")
+                    break
+                }
+                Task { @MainActor in
+                    KvDb.upsertToken(token)
+                    lastPingTime = time()
+                }
+            case let .failure(error):
+                reportLog("ping() failure:\(error)")
+            }
+            isPingInProgress = false
+        }
+    }
+}
