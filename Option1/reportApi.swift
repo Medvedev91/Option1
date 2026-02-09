efed5748763d17931dbfb88f092ae6fb8ed57acb
Ivan <hi@timeto.me>
2026-02-09 07:20:28 +0300

    reportApi.swift: Token

--- Option1/reportApi.swift
+++ Option1/reportApi.swift
@@ -1,16 +1,19 @@
 import Alamofire
 
 func reportApi(_ message: String) {
-    reportLog("reportApi() \(message)")
-    let parameters: [String: String] = [
-        "message": message,
-        "build": SystemInfo.getBuildOrNil().map { "\($0)" } ?? "",
-        "device": SystemInfo.getModelIdentifierOrNil() ?? "",
-        "os": SystemInfo.getOsVersion(),
-    ]
-    _ = AF.request(
-        "https://api.option1.io/report",
-        method: .post,
-        parameters: parameters,
-    ).responseString { _ in }
+    Task { @MainActor in
+        reportLog("reportApi() \(message)")
+        let parameters: [String: String] = [
+            "message": message,
+            "token": KvDb.getTokenOrNil() ?? "",
+            "build": SystemInfo.getBuildOrNil().map { "\($0)" } ?? "",
+            "device": SystemInfo.getModelIdentifierOrNil() ?? "",
+            "os": SystemInfo.getOsVersion(),
+        ]
+        _ = AF.request(
+            "https://api.option1.io/report",
+            method: .post,
+            parameters: parameters,
+        ).responseString { _ in }
+    }
 }
