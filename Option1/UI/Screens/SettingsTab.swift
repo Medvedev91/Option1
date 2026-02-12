b9ee901706199ebfff269729f474166421691c93
Ivan <hi@timeto.me>
2026-02-12 16:37:11 +0300

    SettingsTab.swift:

--- Option1/UI/Screens/SettingsTab.swift
+++ Option1/UI/Screens/SettingsTab.swift
@@ -5,11 +5,19 @@ struct SettingsTab: View {
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 16) {
+                
+                AppText("Website [option1.io](https://option1.io)")
+                
+                AppText("Any Questions [ivan@option1.io](mailto:ivan@option1.io)")
+                
+                AppText("Open Source [https://github.com/Medvedev91/Option1](https://github.com/Medvedev91/Option1)")
+                
                 HStack {
                     SparkleButtonView()
                     Text("v\(SystemInfo.getAppVersionOrNil().map { "\($0)" } ?? "unknown").\(SystemInfo.getBuildOrNil().map { "\($0)" } ?? "unknown")")
                     Spacer()
                 }
+                .padding(.top, 2)
             }
             .padding()
         }
