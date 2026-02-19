510ddb6b0dbefca4a0f4926e6df37de211813f55
Ivan <hi@timeto.me>
2026-02-19 15:37:07 +0300

    DonationsAlertUtils.checkUp()

--- Option1/DonationsAlertUtils.swift
+++ Option1/DonationsAlertUtils.swift
@@ -7,4 +7,33 @@ class DonationsAlertUtils: ObservableObject {
     ///
     
     static let instance = DonationsAlertUtils()
+    
+    @MainActor
+    static func checkUp() -> Bool {
+        let isNeedToShow = calcIsNeedToShow()
+        if isNeedToShow {
+            DonationsAlertUtils.instance.needToShow = true
+        }
+        return isNeedToShow
+    }
+}
+
+@MainActor
+private func calcIsNeedToShow() -> Bool {
+    let now = time()
+    let weekTime = 3_600 * 24 * 7
+    
+    if (launchTime + 3_600) > now {
+        return false
+    }
+    
+    if KvDb.selectActivationTransactionIdOrNil() != nil {
+        return false
+    }
+    
+    if let donationsLastAlertTime = KvDb.selectDonationsLastAlertTimeOrNil() {
+        return (donationsLastAlertTime + weekTime) < now
+    }
+    
+    return (KvDb.selectOrInsertInitTime() + weekTime) < now
 }
