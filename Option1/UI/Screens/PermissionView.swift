138a9c08c4d7260cdb27e4fd917f80c825908646
Ivan <hi@timeto.me>
2026-02-22 22:00:55 +0300

    PermissionView.swift:

--- Option1/UI/Screens/PermissionView.swift
+++ Option1/UI/Screens/PermissionView.swift
@@ -5,14 +5,30 @@ struct PermissionView: View {
     var body: some View {
         VStack {
             
-            Text("Permissions are required to handle hotkeys.")
-                .foregroundColor(.red)
-                .fontWeight(.medium)
-
-            Button("Grant Permissions") {
-                _ = isAccessibilityGranted(showDialog: true)
-            }
-            .padding(.top, 8)
+            Label(
+                " Please allow Option1 handling hotkeys",
+                systemImage: "lightbulb"
+            )
+            .font(.system(size: 28, weight: .medium))
+            
+            Button(
+                action: {
+                    _ = isAccessibilityGranted(showDialog: true)
+                },
+                label: {
+                    Label(
+                        "Open Privacy & Security -> Accessibility Settings",
+                        systemImage: "arrow.right.circle.fill"
+                    )
+                    .font(.system(size: AppText.FONT_SIZE, weight: .medium))
+                }
+            )
+            .padding(.top, 20)
+            
+            Image("permissions")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 800)
         }
     }
 }
