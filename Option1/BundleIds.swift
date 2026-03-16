1a9e45ad5139fa9f1efcceeb315928ac2da8952e
Ivan <hi@timeto.me>
2026-03-16 16:37:00 +0300

    BundleIds.swift:

--- Option1/BundleIds.swift
+++ Option1/BundleIds.swift
@@ -1,5 +1,17 @@
 class BundleIds {
     
-    static let Xcode = "com.apple.dt.Xcode" 
-    static let IntelliJ = "com.jetbrains.intellij" 
+    static let Finder = "com.apple.finder"
+    static let Xcode = "com.apple.dt.Xcode"
+    // JetBrains
+    static let IntelliJ = "com.jetbrains.intellij"
+    // Microsoft
+    static let MicrosoftWord = "com.microsoft.Word"
+    
+    static func isOpenByShellNoNewWindow(_ bundle: String) -> Bool {
+        [Finder, MicrosoftWord].contains(bundle)
+    }
+    
+    static func isOpenByShellWithNewWindow(_ bundle: String) -> Bool {
+        [IntelliJ].contains(bundle)
+    }
 }
