160062f6b541112b399cf8f14b42c1e067c18314
Ivan <hi@timeto.me>
2026-03-25 22:10:21 +0300

    BundleIds.swift: JetBrains IDE's

--- Option1/BundleIds.swift
+++ Option1/BundleIds.swift
@@ -1,5 +1,4 @@
 // todo Apple Pages, Tables...
-// todo Jetbrain IDE's
 // todo Visual Studio
 //
 // При добавлении учитывать UI в WorkspaceBindView.swift
@@ -14,9 +13,21 @@ class BundleIds {
     // JetBrains
     static let IntelliJ = "com.jetbrains.intellij"
     static let PhpStorm = "com.jetbrains.PhpStorm"
+    static let WebStorm = "com.jetbrains.WebStorm"
     static let PyCharm = "com.jetbrains.pycharm"
     static let RustRover = "com.jetbrains.rustrover"
     static let GoLand = "com.jetbrains.goland"
+    static let dotMemory = "com.jetbrains.dotmemory"
+    static let dotTrace = "com.jetbrains.dottrace"
+    static let DataSpell = "com.jetbrains.dataspell"
+    static let DataGrip = "com.jetbrains.datagrip"
+    static let CLion = "com.jetbrains.CLion"
+    static let Rider = "com.jetbrains.rider"
+    static let RubyMine = "com.jetbrains.rubymine"
+    static let SpaceDesktop = "com.jetbrains.space.desktop"
+    static let MPS = "com.jetbrains.mps"
+    static let Gateway = "com.jetbrains.gateway"
+    static let Air = "com.jetbrains.air"
     static let AndroidStudio = "com.google.android.studio"
     // Microsoft
     static let MicrosoftWord = "com.microsoft.Word"
@@ -28,9 +39,21 @@ class BundleIds {
             // JetBrains
             IntelliJ,
             PhpStorm,
+            WebStorm,
             PyCharm,
             RustRover,
             GoLand,
+            dotMemory,
+            dotTrace,
+            DataSpell,
+            DataGrip,
+            CLion,
+            Rider,
+            RubyMine,
+            SpaceDesktop,
+            MPS,
+            Gateway,
+            Air,
             AndroidStudio,
         ].contains(bundle)
     }
