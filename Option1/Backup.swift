943657d1b647961244ac00e0379cac77b7a2e560
Ivan <hi@timeto.me>
2026-03-27 15:49:56 +0300

    Backup.restore()

--- Option1/Backup.swift
+++ Option1/Backup.swift
@@ -1,3 +1,4 @@
+import Foundation
 import SwiftyJSON
 
 class Backup {
@@ -39,5 +40,73 @@ class Backup {
             },
         ])
         return j.rawString(options: .withoutEscapingSlashes)!
+    }
+    
+    @MainActor
+    static func restore(jString: String) throws(AppError) {
+        MenuManager.instance.setWorkspaceDb(nil)
+        
+        let j = JSON(parseJSON: jString)
+        
+        guard let jApps = j["apps"].array else { throw AppError.simple("Json Error") }
+        guard let jKv = j["kv"].array else { throw AppError.simple("Json Error") }
+        guard let jWorkspaces = j["workspaces"].array else { throw AppError.simple("Json Error") }
+        guard let jBinds = j["binds"].array else { throw AppError.simple("Json Error") }
+        
+        for jApp in jApps {
+            guard let bundle: String = jApp["bundle"].string else { throw AppError.simple("Json Error") }
+            guard let name: String = jApp["name"].string else { throw AppError.simple("Json Error") }
+            AppDb.upsertRaw(bundle: bundle, name: name)
+        }
+        
+        do {
+            // Kv
+            KvDb.deleteAll_ForTransaction()
+            for jKv in jKv {
+                guard let key: String = jKv["key"].string else { throw AppError.simple("Json Error") }
+                guard let value: String = jKv["value"].string else { throw AppError.simple("Json Error") }
+                KvDb.insert_ForTransaction(key: key, value: value)
+            }
+            // Workspaces
+            WorkspaceDb.deleteAll_ForTransaction()
+            for jWorkspace in jWorkspaces {
+                guard let uuidString: String = jWorkspace["id"].string else { throw AppError.simple("Json Error") }
+                guard let id = UUID(uuidString: uuidString) else { throw AppError.simple("Json UUID Error") }
+                guard let name: String = jWorkspace["name"].string else { throw AppError.simple("Json Error") }
+                guard let time: Int = jWorkspace["time"].int else { throw AppError.simple("Json Error") }
+                guard let sort: Int = jWorkspace["sort"].int else { throw AppError.simple("Json Error") }
+                WorkspaceDb.insert_ForTransaction(
+                    id: id,
+                    name: name,
+                    date: Date(timeIntervalSince1970: TimeInterval(time)),
+                    sort: sort,
+                )
+            }
+            // Binds
+            BindDb.deleteAll_ForTransaction()
+            for jBind in jBinds {
+                guard let uuidString: String = jBind["id"].string else { throw AppError.simple("Json Error") }
+                guard let id = UUID(uuidString: uuidString) else { throw AppError.simple("Json UUID Error") }
+                guard let key: String = jBind["key"].string else { throw AppError.simple("Json Error") }
+                let workspaceUuidString: String? = jBind["workspace_id"].string
+                let workspaceId: UUID? = workspaceUuidString.map { UUID(uuidString: $0) } ?? nil
+                guard let bundle: String = jBind["bundle"].string else { throw AppError.simple("Json Error") }
+                guard let substring: String = jBind["substring"].string else { throw AppError.simple("Json Error") }
+                BindDb.insert_ForTransaction(
+                    id: id,
+                    key: key,
+                    workspaceId: workspaceId,
+                    bundle: bundle,
+                    substring: substring,
+                )
+            }
+            DB.save()
+        } catch AppError.simple(let message) {
+            DB.rollback()
+            throw AppError.simple(message)
+        } catch {
+            DB.rollback()
+            throw AppError.simple("Error")
+        }
     }
 }
