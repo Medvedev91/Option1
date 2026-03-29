7ce0bca2caed5ec75ebb0193f6de1fca195723ad
Ivan <hi@timeto.me>
2026-03-29 19:23:21 +0300

    Fix closeAppWindow.swift:

--- Option1/closeAppWindow.swift
+++ Option1/closeAppWindow.swift
@@ -1,5 +1,16 @@
 import AppKit
 
 func closeAppWindow() {
-    NSApplication.shared.keyWindow?.close()
+    // Не подходит т.к. закрывает окно приложения
+    // только если если оно на переднем плане.
+    // NSApplication.shared.keyWindow?.close()
+    
+    NSApplication.shared.windows.forEach { window in
+        // Кроме окна приложения есть еще окно Menu Bar
+        // и все закрытие окна Option-Tab (по идее их
+        // их надо удалять но надо разбираться).
+        if window.styleMask.contains(.closable) {
+            window.close()
+        }
+    }
 }
