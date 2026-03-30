af7bf0aaa15946509e49888f3e463447e0b3de3a
Ivan <hi@timeto.me>
2026-03-30 17:03:40 +0300

    OptionTabTab.swift:

--- Option1/UI/Screens/OptionTabTab.swift
+++ Option1/UI/Screens/OptionTabTab.swift
@@ -1,8 +1,22 @@
 import SwiftUI
+import SwiftData
 
 struct OptionTabTab: View {
     
     @State private var isOptionTabEnabled: Bool = OptionTabManager.instance.isEnabled
+    
+    @Query private var appsDb: [AppDb]
+    
+    @Query(sort: \OptionTabPinDb.sort) private var pinsDb: [OptionTabPinDb]
+    private var pinsUi: [PinUi] {
+        pinsDb.map { pinDb in
+            let bundle: String = pinDb.bundle
+            return PinUi(
+                pinDb: pinDb,
+                title: appsDb.first { $0.bundle == bundle }?.name ?? bundle,
+            )
+        }
+    }
     
     var body: some View {
         VStack {
@@ -21,10 +35,55 @@ struct OptionTabTab: View {
             .padding(.horizontal)
             
             List {
-                Text("Efeklj")
-                Text("Iskf;aja")
+                ForEach(pinsUi, id: \.bundle) { pinUi in
+                    HStack {
+                        Text(pinUi.title)
+                        Spacer()
+                        Button(
+                            action: {
+                                OptionTabPinDb.delete(bundle: pinUi.bundle)
+                            },
+                            label: {
+                                Image(systemName: "trash")
+                                    .foregroundColor(.red)
+                            }
+                        )
+                        .buttonStyle(.plain)
+                    }
+                    .padding(.vertical, 4)
+                }
+                .onMove { from, to in
+                    movePins(from: from, to: to)
+                }
             }
         }
         .navigationTitle("Option-Tab")
+    }
+    
+    private func movePins(from: IndexSet, to: Int) {
+        var sortedPinsUi = pinsUi
+        from.forEach { fromIdx in
+            // Fix crash if single item
+            // and ignore same position
+            if fromIdx == to {
+                return
+            }
+            let newFromIdx = fromIdx
+            let newToIdx = (fromIdx > to ? to : (to - 1))
+            sortedPinsUi.swapAt(newFromIdx, newToIdx)
+        }
+        sortedPinsUi.enumerated().forEach { idx, pinUi in
+            pinUi.pinDb.updateSort(idx)
+        }
+    }
+}
+
+private struct PinUi: Hashable {
+    
+    let pinDb: OptionTabPinDb
+    let title: String
+    
+    var bundle: String {
+        pinDb.bundle
     }
 }
