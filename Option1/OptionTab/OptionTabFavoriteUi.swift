9e66e99081c9cfb16d17a8a7e6f0136183e1494c
Ivan <hi@timeto.me>
2026-04-10 16:17:17 +0300

    OptionTabFavoriteUi.swift:

--- Option1/OptionTab/OptionTabFavoriteUi.swift
+++ Option1/OptionTab/OptionTabFavoriteUi.swift
@@ -7,15 +7,21 @@ struct OptionTabFavoriteUi {
     let favoriteDb: FavoriteDb
     let onClick: () -> Void
     
-    var appDb: AppDb? {
-        favoriteDb.selectAppDbOrNil()
-    }
+    let appDb: AppDb?
+    let title: String
+    let icon: NSImage?
     
-    var title: String {
-        favoriteDb.buildUiTitle()
-    }
-    
-    var icon: NSImage? {
-        appDb?.getIcon()
+    init(
+        jumpKey: OptionTabJumpKey,
+        favoriteDb: FavoriteDb,
+        onClick: @escaping () -> Void,
+    ) {
+        self.jumpKey = jumpKey
+        self.favoriteDb = favoriteDb
+        self.onClick = onClick
+        
+        self.appDb = favoriteDb.selectAppDbOrNil()
+        self.title = favoriteDb.buildUiTitle()
+        self.icon = appDb?.getIcon()
     }
 }
